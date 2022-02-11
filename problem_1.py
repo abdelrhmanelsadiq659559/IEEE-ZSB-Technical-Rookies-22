4e6bd73651f8f21613aaf8c8f495da2a4c524ea8
abdelrhmanelsadiq659559 <96255711+abdelrhmanelsadiq659559@users.noreply.github.com>
2022-02-11 18:19:53 +0200

    Create problem_1.py

---
+++ problem_1.py
@@ -0,0 +1,6 @@
+length=int(input("please enter the length of rectangle:"))
+width=int(input("please enter the width of rectangle:"))
+area=length*width
+perimeter=2*(length+width)
+print("the area of rectangle is:",area)
+print("the perimeter of rectangle is:",perimeter)
