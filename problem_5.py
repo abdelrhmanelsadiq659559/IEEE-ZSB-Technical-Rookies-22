b33067f8889b16eb12d023385c1c2551876bd259
abdelrhmanelsadiq659559 <96255711+abdelrhmanelsadiq659559@users.noreply.github.com>
2022-02-11 18:23:29 +0200

    Create problem_5.py

---
+++ problem_5.py
@@ -0,0 +1,12 @@
+array=[1,2,5,6,8,9,6,8,7]
+total=0
+x=len(array)
+for i in range (0,x):
+    total+=array[i]
+print(total)
+
+
+# soultion2
+#array=[1,2,5,6,8,9,6,8,7]
+#sum_array=sum(num)
+#print(sum_arry)
