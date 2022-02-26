3127e637ddf94802c4f77bae8a5bd4f7b4dd4158
abdelrhmanelsadiq659559 <96255711+abdelrhmanelsadiq659559@users.noreply.github.com>
2022-02-26 10:58:22 +0200

    Create ultrasonic.ino

---
+++ Arduino/task4/ultrasonic.ino
@@ -0,0 +1,33 @@
+int trig=13;
+int echo=12;
+long duration;
+int distance;
+
+
+
+void setup() {
+  pinMode(trig,OUTPUT);//send pusle
+  pinMode(echo,INPUT);// recvive it
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  duration=pulseIn(echo,HIGH);
+  distance=duration*.034/2;
+  if (distance >=10)
+  {
+    Serial.println("safe \n");
+  }
+  else 
+  {
+    Serial.println("too close \n");
+  }
+
+
+}
